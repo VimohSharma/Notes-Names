eae0605c29ed0de8548eafe3ada3c5ffb4d4b686
sharmavimoh <91205291+VimohSharma@users.noreply.github.com>
2025-09-05 15:37:16 +0530

    Update NoteRepo.java

--- src/main/java/com/example/Notes/repo/NoteRepo.java
+++ src/main/java/com/example/Notes/repo/NoteRepo.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface NoteRepo extends JpaRepository<Note,Long> {
-    Optional<Note> findBySlug(String slug);
+    
 }
+
